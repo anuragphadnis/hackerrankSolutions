c803ff512e0f688307a87556b32bc3d9bdb41456
abhinavpanse <abhinavpanse@gmail.com>
2017-06-25 11:54:55 +0530

    Update Encryption.java

--- Encryption.java
+++ Encryption.java
@@ -34,9 +34,7 @@ public class Encryption {
         //System.out.println(row);
         int col = (int) Math.ceil ( Math.sqrt( len ) );
         //System.out.println(col);
-        if( row*col > len ) {
-            row++;col++;
-        } 
+         
         //////////////
         int k=0;
         char[][] sol = new char[row][col];
